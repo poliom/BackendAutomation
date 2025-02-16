b0330078450612b0b2c74c752ebff7c20a3b2172
Vidko Videv <vidko33@live.com>
2025-02-16 14:44:06 +0200

    Executable tests, code for overview

---
+++ BackEndAutomation/BackEndAutomation/DataProcessorCodes/DataProcessor.cs
@@ -0,0 +1,30 @@
+﻿namespace BackEndAutomation.DataProcessorCodes
+{
+    public interface IExternalService
+    {
+        string FetchData();
+        string GetData();
+    }
+    public class DataProcessor
+    {
+        private readonly IExternalService _externalService;
+        public DataProcessor(IExternalService externalService)
+        {
+            _externalService = externalService;
+        }
+        public string ProcessData()
+        {
+            var data = _externalService.FetchData();
+            return data.ToUpper();
+        }
+    }
+
+
+    public class DataProcessorParallelTests
+    {
+        public int ProcessData(int input)
+        {
+            return input * 2;
+        }
+    }
+}
---
+++ BackEndAutomation/BackEndAutomation/Tests/RestTests.cs
@@ -0,0 +1,86 @@
+﻿using BackEndAutomation.Rest.Calls;
+using BackEndAutomation.Rest.DataManagement;
+using NUnit.Framework;
+using RestSharp;
+
+namespace BackEndAutomation.Tests
+{
+    [TestFixture]
+    public class RestTests
+    {
+        [Test]
+        public void LoginRestTest()
+        {
+            RestCalls restCalls = new RestCalls();
+
+            RestResponse userDetailsResponse = restCalls.LoginCall("http://161.35.202.130:3000", "dbsdhsh", "sdhshs");
+            Assert.That(userDetailsResponse.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+        }
+
+        [Test]
+        public void LoginRestTestFail()
+        {
+            RestCalls restCalls = new RestCalls();
+
+            RestResponse userDetailsResponse = restCalls.LoginCall("http://161.35.202.130:3000", "dbsdhsh", "sdhasdshs");
+            Assert.That(userDetailsResponse.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+        }
+
+        [Test]
+        public void PersonalDetailsRestTest()
+        {
+            RestCalls restCalls = new RestCalls();
+            ResponseDataExtractors extractResponseData = new ResponseDataExtractors();
+
+            RestResponse userDetailsResponse = restCalls.LoginCall("http://161.35.202.130:3000", "dbsdhsh", "sdhshs");
+            if (userDetailsResponse.StatusCode != System.Net.HttpStatusCode.Created)
+            {
+                Assert.Fail("User is not logged in");
+            }
+
+            string token = extractResponseData.ExtractLoggedInUserToken(userDetailsResponse.Content);
+            string userId = extractResponseData.ExtractUserId(userDetailsResponse.Content).ToString();
+
+            RestResponse userProfileDetailsResponse = restCalls.GetUserPageInformationCall("http://161.35.202.130:3000", userId, token);
+            Assert.That(userProfileDetailsResponse.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void FollowUserRestTest()
+        {
+            RestCalls restCalls = new RestCalls();
+            ResponseDataExtractors extractResponseData = new ResponseDataExtractors();
+
+            RestResponse userDetailsResponse = restCalls.LoginCall("http://161.35.202.130:3000", "dbsdhsh", "sdhshs");
+            if (userDetailsResponse.StatusCode != System.Net.HttpStatusCode.Created)
+            {
+                Assert.Fail("User is not logged in");
+            }
+
+            string token = extractResponseData.ExtractLoggedInUserToken(userDetailsResponse.Content);
+            string userId = extractResponseData.ExtractUserId(userDetailsResponse.Content).ToString();
+
+            RestResponse userFollowResponse = restCalls.ToFollowUser("http://161.35.202.130:3000", "8335", token, true);
+            Assert.That(userFollowResponse.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void UnfollowUserRestTest()
+        {
+            RestCalls restCalls = new RestCalls();
+            ResponseDataExtractors extractResponseData = new ResponseDataExtractors();
+
+            RestResponse userDetailsResponse = restCalls.LoginCall("http://161.35.202.130:3000", "dbsdhsh", "sdhshs");
+            if (userDetailsResponse.StatusCode != System.Net.HttpStatusCode.Created)
+            {
+                Assert.Fail("User is not logged in");
+            }
+
+            string token = extractResponseData.ExtractLoggedInUserToken(userDetailsResponse.Content);
+            string userId = extractResponseData.ExtractUserId(userDetailsResponse.Content).ToString();
+
+            RestResponse userFollowResponse = restCalls.ToFollowUser("http://161.35.202.130:3000", "8335", token, false);
+            Assert.That(userFollowResponse.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+        }
+    }
+}
