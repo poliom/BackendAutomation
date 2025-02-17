271eee9b12f684c69c9b314ae7463ee575e8a88d
Vidko Videv <vidko33@live.com>
2025-02-17 18:08:11 +0200

    Add executable tests

---
+++ BackEndAutomation/BackEndAutomation/Tests/DataProcessorTests.cs
@@ -0,0 +1,41 @@
+﻿using BackEndAutomation.DataProcessorCodes;
+using Moq;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace BackEndAutomation.Tests
+{
+    [TestFixture]
+    public class DataProcessorTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(2 + 2, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void ProcessData_ShouldReturnUpperCaseData()
+        {
+            // Arrange
+            var mockService = new Mock<IExternalService>();
+            mockService.Setup(s => s.FetchData()).Returns("test data");
+            var processor = new DataProcessor(mockService.Object);
+            // Act
+            var result = processor.ProcessData();
+            // Assert
+            Assert.That(result, Is.EqualTo("TEST DATA"));
+        }
+
+        [Test]
+        public void ProcessData_ShouldHandleServiceFailure()
+        {
+            // Arrange
+            var mockService = new Mock<IExternalService>();
+            mockService.Setup(s => s.FetchData()).Throws(new Exception("Service unavailable"));
+            var processor = new DataProcessor(mockService.Object);
+            // Assert
+            Assert.Throws<Exception>(() => processor.ProcessData());
+        }
+    }
+}
